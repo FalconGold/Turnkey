69e9745afcf424caaadcea5cad793e5ebb131f65
FalcFury <yashsoni2017@gmail.com>
2020-06-28 00:31:34 +0530

    Phoenix Pipeline is ready for deployment

--- Phoenix.py
+++ Phoenix.py
@@ -1,17 +1,70 @@
 from lxml import etree
-import io
-import re
-import cloudscraper
 from math import ceil
-import pymongo
 from tqdm import tqdm
+import cloudscraper
+import requests
+import pymongo
+import json
+import re
+import io
 
 
-client = pymongo.MongoClient("localhost", 27017)
+client = pymongo.MongoClient("YOUR_MONGODB_LINK_HERE")
 db = client.automation_zillow
 col = db.data
-db2 = client.final_zillow
-col2 = db2.data
+
+def get_sales_comps(zipcode):
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(f"https://www.redfin.com/zipcode/{zipcode}/housing-market")
+    print(response.status_code)
+    parser = etree.HTMLParser(encoding="utf-8")
+    doc = etree.parse(io.StringIO(str(response.text)), parser)
+    sc = 0
+    if "K" in "".join(doc.xpath("///div[@class='detail saleSqft']/div/div[@class='value']/span/text()")).replace("$",""):
+        sc = float("".join(doc.xpath("///div[@class='detail saleSqft']/div/div[@class='value']/span/text()")).replace("$","").replace("K",""))*1000
+    else:
+        sc = "".join(doc.xpath("///div[@class='detail saleSqft']/div/div[@class='value']/span/text()")).replace("$","")
+    print(sc)
+    return int(sc)
+
+
+def get_financial_model(val):
+    try:
+        if val['on_off'] == "On":
+            price = val['price']
+            # print
+        else:
+            price = 0
+    except Exception as e:
+        pass  # print(e)
+    bldsize = val['buildingSize']
+    buil = val['buildable']
+    existing = bldsize * 0
+    buildable = val['buildable'] * 150
+    tsf = bldsize + buil  # bldsize + lotsize*far - bldsize
+    total_of_existing_and_building = existing + buildable
+    try:
+        acquition = price * 0.8
+    except:
+        acquition = 0  # print(new['address'][i])
+    refurbishment = (existing + buildable) * 0.8
+    total_of_acquition_refurbishment = acquition + refurbishment
+    arrangement_fee = total_of_acquition_refurbishment * 0.01
+    interest_charge = total_of_acquition_refurbishment * 0.05
+    equity = (price + total_of_existing_and_building) - total_of_acquition_refurbishment
+    total_source_of_funds = (price + total_of_existing_and_building + total_of_acquition_refurbishment) - total_of_acquition_refurbishment
+    total_financing_and_closing_cost = arrangement_fee + total_of_existing_and_building + interest_charge
+    total_project_cost = arrangement_fee + total_of_existing_and_building + interest_charge + price
+    total_cost_of_property = price + total_of_existing_and_building
+    equity_required = (price + total_of_existing_and_building) - total_of_acquition_refurbishment
+    total_sr = (tsf * val['salesComp']) - (tsf * val['salesComp'] * 0.03)
+    total_debt_service = total_of_acquition_refurbishment + interest_charge + equity + arrangement_fee
+    profit = total_sr - total_debt_service
+    # print(profit)
+    val["profit"] = round(float(profit), 2)
+    val["brokerFees"] = (tsf * val['salesComp'] * 0.03)
+    return val
+
 
 def get_data(item):
     scraper = cloudscraper.create_scraper()
@@ -29,15 +82,24 @@ def get_data(item):
         try:
             val['address'] = js2[list(js2.keys())[0]]['property']['streetAddress'] + ", " + js2[list(js2.keys())[0]]['property']['city'] + ", " + js2[list(js2.keys())[0]]['property']['state'] + " " + js2[list(js2.keys())[0]]['property']['zipcode']
         except:
-            val['address'] = ""
+            val['address'] = " "
         try:
             val['price'] = js2[list(js2.keys())[0]]['property']['price']
         except:
-            val['price'] = ""
+            val['price'] = 0
         try:
             val['zipcode'] = js2[list(js2.keys())[0]]['property']['zipcode']
         except:
             val['zipcode'] = ""
+
+        try:
+            val['salesComp'] = 253 # get_sales_comps(js2[list(js2.keys())[0]]['property']['zipcode'])
+        except:
+            val['salesComp'] = 0
+        try:
+            val['lotsize'] = js2[list(js2.keys())[0]]['property']['lotSize']
+        except:
+            val['lotsize'] = 0
 
         try:
             val['image'] = js2[list(js2.keys())[0]]['property']['tvCollectionImageLink']
@@ -64,12 +126,59 @@ def get_data(item):
                 val['phoneNumber'] = ", ".join(pn)
             except:
                 pass
+
         val['url'] = item
-        return val
+        return val, js2[list(js2.keys())[0]]['property']['streetAddress'], js2[list(js2.keys())[0]]['property']['city'] + ", " + js2[list(js2.keys())[0]]['property']['state']
     except:
         return False
 
 
+def get_zoning_data(val, addr, addr2):
+    zoning_value = {
+        "RE-35": 30,
+        "R1-18": 25,
+        "R1-10": 50,
+        "R1-8": 50,
+        "R1-6": 50,
+        "R-2": 50,
+        "R-3": 50,
+        "R-3A": 50,
+        "R-4": 50,
+        "R-5": 50,
+    }
+    headers = {"apikey": "aa5a8218087e1dfcf7a262d1ab615e5f", "accept": "application/json"}
+    response = requests.get(
+        f"https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/expandedprofile?address1={addr}&address2={addr2}",
+        headers=headers)
+    js = json.loads(response.text)
+    try:
+        val['zoning'] = js['property'][0]['lot']['siteZoningIdent']
+    except:
+        val['zoning'] = " "
+
+    try:
+        val['buildingSize'] = js['property'][0]['building']['size']['bldgSize']
+    except:
+        val['buildingSize'] = 0
+    try:
+        val['level'] = js['property'][0]['building']['summary']['levels']
+    except:
+        val['level'] = 0
+
+    try:
+        val['buildable'] = (val['lotsize'] * (zoning_value[val['zoning']]/100)) - val['buildingSize']
+    except:
+        val['buildable'] = 0
+
+    val['on_off'] = "On"
+    val['status'] = "For Sale"
+    val['profit'] = 0
+    val['disable'] = "F"
+    val['brokerFees'] = 0
+    val['agentEmail'] = " "
+    val['far'] = 0
+    val['agentCompany'] = " "
+    return val
 
 
 def get_address(url, current_page, total):
@@ -104,77 +213,25 @@ def get_address(url, current_page, total):
                     # val = {}
                     try:
                         # print(div.xpath(".//a[contains(@class,'list-card-link')]/@href")[0])
-                        val = get_data(div.xpath(".//a[contains(@class,'list-card-link')]/@href")[0])
+                        val, addr, addr2 = get_data(div.xpath(".//a[contains(@class,'list-card-link')]/@href")[0])
+                        val = get_zoning_data(val, addr, addr2)
+                        val = get_financial_model(val)
+                        # print(val)
                         if val != False:
-                            col.insert_one(val)
-                    except:
-                        pass
-                print("Going through the LAC")
+                            if val['profit'] > 0:
+                                col.insert_one(val)
+                    except Exception as e:
+                        print(e)
                 if stop:
                     pass
                 else:
-                    print("Got Past LAC")
-                    print(f"Values retained are | Current Page: {current_page+1}, result_count: {result_count}")
                     get_address(url, current_page+1, result_count)
         except:
             pass
     else:
         pass
 
-def get_final_zillow(zipcode):
-    mydoc = col.find({"zipcode": zipcode})
-    for x in tqdm(mydoc):
-        scraper = cloudscraper.create_scraper()
-        response = scraper.get(x['url'])
-        parser = etree.HTMLParser(encoding="UTF-8")
-        doc = etree.parse(io.StringIO(str(response.text)), parser)
-        try:
-            js = eval([ld for ld in doc.xpath("//script[@type='application/json']/text()") if "apiCache" in ld][0])
-            false = False
-            true = True
-            null = ""
-            js2 = eval(js['apiCache'])
-            # list(js2.keys())[0]
-            val = {}
-            try:
-                val['Indicates the lot size, in square feet'] = js2[list(js2.keys())[0]]['property']['lotSize']
-            except:
-                val['Indicates the lot size, in square feet'] = 0
-            try:
-                val['zipcode'] = zipcode
-            except:
-                val['zipcode'] = ""
-            try:
-                val['Building size'] = js2[list(js2.keys())[0]]['property']['livingArea']
-            except:
-                val['Building size'] = 0
-            try:
-                val['Total Market Value'] = js2[list(js2.keys())[0]]['property']['price']
-            except:
-                val['Total Market Value'] = " "
-            try:
-                val['address'] = js2[list(js2.keys())[0]]['property']['streetAddress'] + ", " + \
-                                 js2[list(js2.keys())[0]]['property']['city'] + ", " + \
-                                 js2[list(js2.keys())[0]]['property']['state'] + " " + \
-                                 js2[list(js2.keys())[0]]['property']['zipcode']
-            except:
-                val['address'] = " "
-            try:
-                val['propLandUse'] = js2[list(js2.keys())[0]]['property']['homeType'].replace("_", " ")
-            except:
-                val['propLandUse'] = " "
-            col2.insert_one(val)
-
-        except:
-            pass
-
-
-
 
 if __name__ == "__main__":
-    url = "https://www.zillow.com/scottsdale-az-85258/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-111.95348392114256%2C%22east%22%3A-111.81855807885741%2C%22south%22%3A33.51559616742752%2C%22north%22%3A33.60534316012991%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A94850%2C%22regionType%22%3A7%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22mapZoom%22%3A13%2C%22filterState%22%3A%7B%22con%22%3A%7B%22value%22%3Afalse%7D%2C%22apa%22%3A%7B%22value%22%3Afalse%7D%2C%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C%22tow%22%3A%7B%22value%22%3Afalse%7D%2C%22manu%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
+    url = "https://www.zillow.com/phoenix-az-85033/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-112.24306496057129%2C%22east%22%3A-112.17560203942871%2C%22south%22%3A33.469551848206464%2C%22north%22%3A33.514460890590044%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A94749%2C%22regionType%22%3A7%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22mapZoom%22%3A14%2C%22filterState%22%3A%7B%22con%22%3A%7B%22value%22%3Afalse%7D%2C%22apa%22%3A%7B%22value%22%3Afalse%7D%2C%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C%22land%22%3A%7B%22value%22%3Afalse%7D%2C%22tow%22%3A%7B%22value%22%3Afalse%7D%2C%22manu%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
     get_address(url, 1, 0)
-    zipcode = "".join(re.findall("\d+", url.split("/?")[0]))
-    print("Getting the Data From Zillow")
-    if zipcode.isdigit():
-        get_final_zillow(zipcode)
